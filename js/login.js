c1498b2fd766626cddda9a360d671ed9e4ffc78d
romaindbz <rom_debouze@msn.com>
2014-12-14 17:23:25 +0100

    Liste des parties
    Liste des partie cliquable pour rejoindre

--- js/login.js
+++ js/login.js
@@ -5,9 +5,12 @@ function init() {
 		var ul = document.getElementById('lesParties');
 		ul.innerHTML = '';
 		for (p in data.gamesList) {
-			var li = document.createElement('li');
-			ul.appendChild(li);
-			li.appendChild(document.createTextNode(data.gamesList[p]));
+			document.getElementById('newForm').innerHTML = document.getElementById('newForm').innerHTML +"<form method=\"post\" action=\"\" id=\"partie"+data.gamesList[p]+"\" role=\"form\"><input type=\"hidden\" class=\"form-control\" id=\"login\" name=\"login\" placeholder=\"Entrez un pseudo\" value=\"unknow\"/><input type=\"hidden\" class=\"form-control\" id=\"idGame\" name=\"idGame\" placeholder=\"Nom de la partie\" value =\""+data.gamesList[p]+"\"/><button type=\"submit\" style=\"background-color: #2C3E50; border:none\">"+data.gamesList[p]+"</button></form>";
+
+			//var li = document.createElement('li');
+			//ul.appendChild(li);
+			//li.appendChild(document.createTextNode('<a href="#" onclick="document.getElementById(\'nouvellePartie2\').submit; return false;">'+document.createTextNode(data.gamesList[p])+'</a>'));
+			//li.appendChild(document.createTextNode(data.gamesList[p]));
 		}
 	});
 	socket.emit('loginPage');
